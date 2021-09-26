65513e9c4ad94e290988214b022784fef32dc7ca
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-09-26 18:39:43 -0300

    feat: add start script

--- src/app/routes/index.routes.js
+++ src/app/routes/index.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+
 import sessionRouter from "./session.routes"
 import userRouter from "./user.routes"
 const routes = Router();
-
+routes.get("/", (_, res) => {
+    return res.json({ status: "up" })
+})
 routes.use("/sessions", sessionRouter)
 routes.use("/users", userRouter)
 
