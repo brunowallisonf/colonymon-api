88955d5dac7b10aee9ac527fbcda79b000c7f4f8
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-09-26 18:21:50 -0300

    feat: inital heroku commit

--- src/database/index.js
+++ src/database/index.js
@@ -2,15 +2,12 @@ import Sequelize from "sequelize"
 import databaseConfig from "../config/database"
 import User from "../app/models/User"
 
-
 const models = [User]
-
 class Database {
     constructor() {
         this.init()
     }
     init() {
-
         this.connection = new Sequelize(databaseConfig[process.env.NODE_ENV || "development"])
         this.connection
             .authenticate()
