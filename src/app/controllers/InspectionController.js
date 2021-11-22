bcc22f34c3dd4f31c23d0131999cb35bbda11e21
Bruno Wallison <brunowallison.fernandes@gmail.com>
2021-11-22 09:23:06 -0300

    feat: add list inspections endpoint

--- src/app/controllers/InspectionController.js
+++ src/app/controllers/InspectionController.js
@@ -8,9 +8,12 @@ class InspectionController {
     }
 
     async list(req, res) {
+        const limit = 10
+        const page = parseInt(req.query.page || 1)
+        const offset = (page - 1) * limit
         const { userId } = req;
-        const inspections = await Inspection.findAll({ where: { userId } })
-        return res.json(inspections)
+        const inspections = await Inspection.findAll({ where: { userId }, order: [["inspectionDate", "DESC"]], limit, offset })
+        return res.json({ page, inspections })
     }
 }
 
